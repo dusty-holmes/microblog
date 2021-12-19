37d0a9be90ecffdbe41a695a6ce31d0276035c7c
Dusty <dustin.holmes133@gmail.com>
2021-12-19 16:42:56 -0600

    added HTML code to index() function

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Dusty'}
+    return return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>''''''
+<html>
+    <head
+</html>
+'''
