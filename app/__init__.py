cc0e6f7ecaf846b86e22aa64a4bda391e19495f1
Dusty <dustin.holmes133@gmail.com>
2021-12-20 14:16:46 -0600

    added logging functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.logger.setLevel(logging.INFO)
 
 from app import routes
