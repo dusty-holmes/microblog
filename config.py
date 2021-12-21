851e13b4de4c5efb5d71455aed7618250a3bb80e
Dusty <dustin.holmes133@gmail.com>
2021-12-21 09:31:57 -0600

    Added SQLAlchemy Config tags Tags stopped compiler from throwing warnings when running app

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SOME SECRET KEY 01'
-    SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqllite:///' + os.path.join(base_dir, 'app.db')
