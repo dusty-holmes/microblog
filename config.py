ed55c79cb06b3a731deb3302e554d5d4bce710e9
Dusty <dustin.holmes133@gmail.com>
2021-12-21 09:10:31 -0600

    Added DB, SQLAlchemy objects and imports

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SOME SECRET KEY 01'
+    SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqllite:///' + os.path.join(base_dir, 'app.db')
